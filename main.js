8820d3f21fc00f23e8cb34948aed736b5f20407d
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-03-28 09:05:44 +1100

    added navigation dots

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const slides = document.getElementsByClassName("slide");
 const prev = document.getElementsByClassName("prev")[0];
 const next = document.getElementsByClassName("next")[0];
-
+const dots = Array.from(document.getElementsByClassName("dot"));
 slideIndex = 0;
 showSlide(slideIndex);
 
@@ -13,13 +13,25 @@ next.addEventListener("click", () => {
   showSlide((slideIndex += 1));
 });
 
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    showSlide((slideIndex = index));
+  });
+});
+
 function showSlide(n) {
-  console.log(slideIndex);
+  // console.log(slideIndex);
   if (n >= slides.length) slideIndex = 0;
   if (n < 0) slideIndex = slides.length;
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
   slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
 }
