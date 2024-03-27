47da94d1a94e7470a45fc132ac207328809561d5
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-03-28 09:21:32 +1100

    added some animation

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ dots.forEach((dot, index) => {
 function showSlide(n) {
   // console.log(slideIndex);
   if (n >= slides.length) slideIndex = 0;
-  if (n < 0) slideIndex = slides.length;
+  if (n < 0) slideIndex = slides.length - 1;
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
